f440ced59b1054f9832e0fc6fd4668a1b81ebd36
吳元皓 <157942818+hpware@users.noreply.github.com>
2024-03-05 21:01:11 +0800

    Create iis-redirect.php

---
+++ iis-redirect.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<meta charset="UTF-8" />
+<meta http-equiv="refresh" content="5; url=https://iis.hwtwco.uk" />
+<script type="text/javascript">
+  window.location.href = "https://iis.hwtwco.uk";
+</script>
+<title>Redirect to Github</title>
+<!-- Note: don't tell people to `click` the link, just tell them that it is a link. -->
+If you are not redirected automatically, follow this <a href="https://iis.hwtwco.uk/">Link</a>
